b7a0af1a5bdb31a40e1ff597f507aed98c22bcd9
unknown <marvinraydalida@gmail.com>
2024-03-24 22:56:00 +0800

    demo complete

--- pages/demo.vue
+++ pages/demo.vue
@@ -6,11 +6,11 @@
         <a-button @click="searchMovie">Search</a-button>
     </a-flex>
     <!-- <span v-if="result">{{ result.Search }}</span> -->
-    <a-row v-if="result" :gutter="[10,10]" style="margin-top: 50px;">
+    <!-- <a-row v-if="result" :gutter="[10,10]" style="margin-top: 50px;">
         <a-col v-for="movie in result.Search" :key="movie.imdbID">
             <Poster :title="movie.Title" :id="movie.imdbID" :poster="movie.Poster"/>
         </a-col>
-    </a-row>
+    </a-row> -->
 </template>
 
 <script setup>
@@ -23,11 +23,8 @@
 
     const searchMovie = async () => {
         const response = await fetch(`http://www.omdbapi.com/?apikey=3eae3080&s=${title.value}`)
-        console.log(response)
         const value = await response.json()
-        console.log(value)
         result.value = value
-        if(result.value)
         console.log(result.value.Search)
     }
 
